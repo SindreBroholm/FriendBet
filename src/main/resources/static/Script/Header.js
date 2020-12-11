88ff037c31db32fd06f5305cb8ad36f653d62ecb
SindreBroholm <sindreset@gmail.com>
2020-12-11 15:59:15 +0100

    save

--- src/main/resources/static/Script/Header.js
+++ src/main/resources/static/Script/Header.js
@@ -4,60 +4,74 @@ let socket = new SockJS('/websocketApp');
 let stompClient = Stomp.over(socket);
 stompClient.connect({}, connectionSuccess);
 let notificationCount = 0;
+let url = location.href.split("/");
 function connectionSuccess() {
-    stompClient.subscribe('/topic/chat', onMessageReceived);
+    if (url[3] === 'room'){
+        stompClient.subscribe('/topic/chat', onMessageReceived);
+    }
     stompClient.subscribe('/topic/notification', onNotificationReceived);
 }
 
 //get message from backend and present it to view.
 function onNotificationReceived(payload) {
+    let NotificationAlertNode = document.getElementById('notificationBox');
+    NotificationAlertNode.style.background = '#DB7C88';
     notificationCount++;
+
+
+
     let notification = JSON.parse(payload.body);
     let notificationElement = document.createElement('li');
+
     notificationElement.classList.add('notification-data');
-
-
     let notificationDiv = document.createElement('div');
     notificationDiv.classList.add('recipientUser');
     let notificationContentNode = document.createElement('p');
-    let notificationContent = document.createTextNode(notification.content);
+    notificationContentNode.style.background = '#DB7C88';
 
+    let notificationContent = document.createTextNode(notification.content);
     notificationContentNode.appendChild(notificationContent);
+
     notificationDiv.appendChild(notificationContentNode);
     notificationElement.appendChild(notificationDiv);
 
     document.querySelector('#NotificationList').appendChild(notificationElement);
-    document.querySelector('#NotificationList').scrollTop = document
-        .querySelector('#messageList').scrollHeight;
 
-    notificationContentNode.style.background = '#1D9F91';
+
 
     notificationContentNode.addEventListener('click', function (event) {
         notificationCount--;
-        notificationContentNode.style.background = '#DB7C88';
+        notificationContentNode.style.background = '#1D9F91';
     });
+
+    NotificationAlertNode.addEventListener('click', function (){
+        NotificationAlertNode.style.background = '#1D9F91';
+        document.querySelector('#NotificationList').scrollTop = document
+            .querySelector('#NotificationList').scrollHeight;
+
+    })
 }
 
 function showDropDownMenu(ele) {
     let buttonNode = ele;
-    let DDM = document.getElementById("dropDownLinks")
+    let menuDropDown = document.getElementById("dropDownLinks")
     buttonNode.addEventListener('click', function () {
-        if (DDM.style.display === 'none') {
-            DDM.style.display = 'block';
+        if (menuDropDown.style.display === 'none') {
+            menuDropDown.style.display = 'block';
         } else {
-            DDM.style.display = 'none';
+            menuDropDown.style.display = 'none';
         }
     });
 }
 
 function showNotification(ele) {
     let buttonNode = ele;
-    let DDM = document.getElementById("NotificationList")
+    let notifyDropDown = document.getElementById("NotificationList")
     buttonNode.addEventListener('click', function () {
-        if (DDM.style.display === 'none') {
-            DDM.style.display = 'block';
+        if (notifyDropDown.style.display === 'none') {
+            notifyDropDown.style.display = 'block';
         } else {
-            DDM.style.display = 'none';
+            notifyDropDown.style.display = 'none';
         }
     });
 }
